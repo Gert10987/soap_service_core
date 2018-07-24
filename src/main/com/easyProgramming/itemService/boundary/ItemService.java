a8ef9155cb1dcf25d63b55ff9cd9b3a212156c49
pawel <pawel.kalasz@gmail.com>
2018-07-24 20:42:20 +0200

    Changed file name

--- src/main/com/easyProgramming/itemService/boundary/ItemService.java
+++ src/main/com/easyProgramming/itemService/boundary/ItemService.java
@@ -1,10 +1,11 @@
 package com.easyProgramming.itemService.boundary;
 
-import com.easyProgramming.itemService.control.ItemServiceControl;
 import com.easyProgramming.itemService.model.Item;
+import com.easyProgramming.itemService.model.ItemServiceRegisterResponse;
 import com.easyProgramming.itemService.repository.Component;
 import com.easyProgramming.itemService.repository.IItemRepository;
 
+import javax.ejb.Stateful;
 import javax.inject.Inject;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -16,16 +17,23 @@ import javax.jws.soap.SOAPBinding;
         portName = "ItemService",
         targetNamespace = "http://easyProgramming.com")
 @SOAPBinding(style = SOAPBinding.Style.RPC)
-public class IItemService implements IItemRepository {
+public class ItemService implements IItemRepository {
 
     @Inject
     @Component
-    private ItemServiceControl itemServiceControl;
+    private IItemRepository itemServiceControl;
 
     @WebMethod(operationName = "checkItem")
     @WebResult(name = "Item")
     @Override
     public Item checkItem(@WebParam(name = "itemId") Long id) {
         return itemServiceControl.checkItem(id);
+    }
+
+    @WebMethod(operationName = "insertItem")
+    @WebResult(name = "ItemServiceResponse")
+    @Override
+    public ItemServiceRegisterResponse registerItem(@WebParam(name = "item") Item item) {
+        return itemServiceControl.registerItem(item);
     }
 }
